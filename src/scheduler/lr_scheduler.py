6b5a2b682110e3f88ec17ebb0d3b4c9677ade537
Riccardo Musmeci <riccardomusmeci@ESMAD10L1583788.enelint.global>
2022-09-10 11:26:08 +0200

    refactoring

---
+++ src/scheduler/lr_scheduler.py
@@ -0,0 +1,32 @@
+from typing import Dict
+from torch.optim import Optimizer
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
+from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR, _LRScheduler
+
+def lr_scheduler(
+    optimizer: Optimizer, 
+    name: str,
+    **kwargs
+) -> _LRScheduler:
+    """returns lr scheduler
+
+    Args:
+        optimizer (Optimizer): optimizer
+        scheduler (str): which scheduler
+
+    Returns:
+        _LRScheduler: scheduler
+    """
+    support = ["linear_cosine", "cosine"]
+    assert name in support, f"Only {support} schedulers are supported."
+    
+    if name == "linear_cosine":
+        return LinearWarmupCosineAnnealingLR(
+            optimizer=optimizer,
+            **kwargs
+        )
+    if name == "cosine":
+        return CosineAnnealingWarmRestarts(
+            optimizer=optimizer,
+            **kwargs
+        )
