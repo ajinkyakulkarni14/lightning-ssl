9ff6c93d22b01031d07b10d7434015a113e5926e
Riccardo Musmeci <riccardomusmeci@ESMAD10L1583788.enelint.global>
2022-09-10 19:26:03 +0200

    STL10 supports image-folder structure

--- src/dataset/ssl_stl10.py
+++ src/dataset/ssl_stl10.py
@@ -24,22 +24,31 @@ class SSLSTL10(Dataset):
         if not os.path.exists(root):
             print(f"{root} does not exists. Quitting.")
             quit()
-            
+        
         self.data_dir = os.path.join(root, "unlabeled" if train else "test")
-        self.img_paths = [os.path.join(self.data_dir, f) for f in os.listdir(self.data_dir) if not f.startswith(".")]
-        if not train:
-            self.classes = [int(f.split("_")[-1].split(".")[0]) for f in os.listdir(self.data_dir) if not f.startswith(".")]
+        if train:
+            self.img_paths = [os.path.join(self.data_dir, f) for f in os.listdir(self.data_dir) if not f.startswith(".")]
+            self.targets = None
         else:
-            self.classes = None
+            classes = [c for c in os.listdir(self.data_dir) if not c.startswith(".")]
+            self.targets = []
+            self.img_paths = []
+            for c_i, c in enumerate(classes):
+                c_dir = os.path.join(self.data_dir, c)
+                for f in os.listdir(c_dir):
+                    if not f.startswith("."):
+                        self.img_paths.append(os.path.join(self.data_dir, c, f))
+                        self.targets.append(c_i)
+                        
         self.transform = transform
         
     def __getitem__(self, index) -> Tuple:
         
         img_path = self.img_paths[index]
-        if self.classes is not None:
-            label = self.classes[index]
+        if self.targets is not None:
+            label = self.targets[index]
         else:
-            label = None
+            label = 0 # just to keep collate_fn simple
             
         img = read_rgb(img_path)
         views = None
