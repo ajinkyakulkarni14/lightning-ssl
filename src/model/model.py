75a18415988c800f642164cbe6b85937ff6c46f5
Riccardo Musmeci <riccardomusmeci@ESMAD10L1583788.enelint.global>
2022-09-10 11:10:20 +0200

    better loading ssl models

---
+++ src/model/model.py
@@ -0,0 +1,16 @@
+from typing import Union
+from src.model.byol import BYOL
+from src.model.dino import DINO
+
+def ssl_model(
+    model: str,
+    **kwargs
+) -> Union[BYOL, DINO]:
+    
+    if model == "byol":
+        return BYOL(**kwargs)
+    if model == "dino":
+        return DINO(**kwargs)
+    
+    print(f"{model} not supported.")
+    quit()
